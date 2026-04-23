d26967f5397c207170011e150b7351c59ba581bb
MariaLuizaSantana1 <malunasa21@gmail.com>
2026-04-23 13:16:03 -0300

    get e set para cd.java

--- src/main/java/br/edu/ifpb/poo/modelo/Cd.java
+++ src/main/java/br/edu/ifpb/poo/modelo/Cd.java
@@ -3,4 +3,19 @@ package br.edu.ifpb.poo.modelo;
 public class Cd {
     private String artista;
     private String[] listaFaixas;
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+    public String getArtista() {
+        return artista;
+    }
+
+
+    public void setListaFaixas(String[] listaFaixas) {
+        this.listaFaixas = listaFaixas;
+    }
+    public String[] getListaFaixas() {
+        return listaFaixas;
+    }
 }
