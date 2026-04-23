0e4a9c3ad0dcd6d805056b43a1abfb77e9638201
MariaLuizaSantana1 <malunasa21@gmail.com>
2026-04-22 23:04:17 -0300

    get e set

--- src/main/java/br/edu/ifpb/poo/modelo/Dvd.java
+++ src/main/java/br/edu/ifpb/poo/modelo/Dvd.java
@@ -4,4 +4,29 @@ public class Dvd {
     private String diretor;
     private int duracao;
     private char classificacaoIndicativa;
+
+
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+    public String getDiretor() {
+        return diretor;
+    }
+
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+    public int getDuracao() {
+        return duracao;
+    }
+
+
+    public void setClassificacaoIndicativa(char classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+    public char getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
 }
